f45566b409657ea65e9135138e0484464f8a816c
Reyyan Arik <reyyanbusraarik92@gmail.com>
2019-03-01 21:58:12 +0300

    skipSplashScreen method is opened

--- src/test/java/com/appium/nesine/test/FilterActivityAndAddToBasketTest.java
+++ src/test/java/com/appium/nesine/test/FilterActivityAndAddToBasketTest.java
@@ -8,7 +8,7 @@ public class FilterActivityAndAddToBasketTest extends BaseTest{
 		
 	@Test
 	public void filterActivityAndAddToBasketTest() {
-		//splashScreen.skipSplashScreen();
+		splashScreen.skipSplashScreen();
 		mainScreen.clickFootballMenu();
 		footballScreen.checkAndClickFilterButton();
 		filterScreen.clickLeagueSelectionMenu();
